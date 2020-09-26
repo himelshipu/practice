dee5ff9f2c7e1db3e465186bf2699e7bba76782a
Himelshipu <shipuhimel@gmail.com>
2020-09-27 00:47:04 +0600

    checking simple laravel features

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'age' => \App\Http\Middleware\CheckAge::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
@@ -63,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+/*Route::get('/home', function () {
+    return view('welcome');
+});*/
+
+Route::get('/service', function () {
+    return view('service');
+});
+
+Route::get('/about', function () {
+    return view('about');
+})->middleware('age');
+
+Route::get('/all_contact', function () {
+    return view('contact');
+})->name('contact');
+
+
+Route::get('/all_contact','ContactController@contact')->name('contact');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
